5cbcaa0d98dd26bd44c8ba4d1141ca800047ebc6
Rheatrias <francescobredariol03@gmail.com>
2026-09-25 16:23:57 +0200

    osu_benchmark : added the ifniniband reference

--- sbatch_files/osu_benchmark.sh
+++ sbatch_files/osu_benchmark.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=16
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=60G
-#SBATCH --time=00:20:00
+#SBATCH --time=01:00:00
 #SBATCH --output=osu_benchmark.out
 
 module load singularity/4.3.1
@@ -20,10 +20,10 @@ REAL_SCRATCH=$(pwd -P)
 CONTAINER=~/scratch/sample_sort.sif
 
 NATIVE_OSU=~/scratch/osu-micro-benchmarks-7.4/c/mpi/collective/blocking/osu_alltoallv
-CONTAINER_OSU=/opt/osu/libexec/osu-micro-benchmarks/mpi/collective/blocking/osu_alltoallv
+CONTAINER_OSU=/opt/osu/libexec/osu-micro-benchmarks/mpi/collective/osu_alltoallv
 
 echo "Running Native OSU Alltoallv Benchmark"
-srun -n 16 $NATIVE_OSU -m 1024:67108864 > ~/scratch/osu_native.out
+srun -n 16 $NATIVE_OSU -m 1024:67108864 -M 200000000 > ~/scratch/osu_native.out
 
 echo "Running Containerized OSU Alltoallv Benchmark"
-srun -n 16 singularity exec -B /orfeo --pwd $REAL_SCRATCH $CONTAINER $CONTAINER_OSU -m 1024:67108864 > ~/scratch/osu_container.out
+srun -n 16 singularity exec -B /dev/infiniband,/orfeo --pwd $REAL_SCRATCH $CONTAINER $CONTAINER_OSU -m 1024:67108864 -M 200000000 > ~/scratch/osu_container.out
